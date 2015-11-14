4c8938a939680b1799f57114bd25df63253ace73
mo valipour <valipour@gmail.com>
2015-11-14 00:52:06 +0000

    add logging

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ var token = '160579290:AAENI_8O1JMXwkWR30VJ9Bnjo6wLZlZI0jk';
 var Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true });
 
+console.log('bot server started...');
+
 // hello command
 bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
   var name = match[1];
