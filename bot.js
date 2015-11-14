1d29f0557b8a7d6bebab8a7141d6cfb4ab3c9157
mo valipour <valipour@gmail.com>
2015-11-14 00:29:25 +0000

    initial commit

---
+++ bot.js
@@ -0,0 +1,23 @@
+var token = '160579290:AAENI_8O1JMXwkWR30VJ9Bnjo6wLZlZI0jk';
+
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+// hello command
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+// sum command
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
+});
